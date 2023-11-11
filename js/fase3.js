7e574b97ee0068797acb8630803cc3b3430962d5
Vittor Patricio <121522773+VittorPatricio@users.noreply.github.com>
2023-11-11 14:33:11 -0300

    Update fase3.js
    adicionando vibração no acerto

--- js/fase3.js
+++ js/fase3.js
@@ -80,6 +80,7 @@ const checkCards = () =>{
         const matchSound = document.getElementById('matchSound');
         matchSound.play();
 
+        window.navigator.vibrate(550)
 
         firstCard = '';
         secondCard = '';
