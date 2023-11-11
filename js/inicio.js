e958a8bc1e5e2a1f014a0f424c83b76b6bc15c38
VittorPatricio <vittorpatricio26@gmail.com>
2023-11-11 17:54:13 -0300

    modal tela inicial

--- js/inicio.js
+++ js/inicio.js
@@ -1,13 +1,17 @@
-const form = document.getElementById("exampleModal");
-const imagem = document.getElementById("logo");
+if (document.getElementById("logo").classList.contains("class-logo")) {
+    // A classe foi adicionada
+  } else {
+    // A classe não foi adicionada
+  }
+
+function inicar() {
+    console.log("imagemaaaaa")
+    document.getElementById("logo").classList.add("class-logo");
+}
 
 function jogar(){
     console.log("oiii")
     window.location.href = "fase1.html";
-}
-
-function fechar() {
-    form.style.display = "none";
 }
 
 window.onload = function() {
